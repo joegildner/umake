53b1a254e92605e2ec0c8bc4b7ee55a9c8fd3214
Joe Gildner <joegildner0@gmail.com>
2018-11-08 11:24:27 -0800

    state machine is mostly functional, putting aside for review before I make it needlessly complicated

--- src/linetype.c
+++ src/linetype.c
@@ -8,6 +8,8 @@
 int linetype(char* thischar);
 int linetype_rule(char* thischar);
 int linetype_targ(char* thischar);
+void printchars(char* chars);
+bool validchar(char thischar, int charset);
 
 enum{
 	LINE_INVALID = -1,
@@ -15,14 +17,17 @@ enum{
 	LINE_RULE
 };
 
+char rulechars[4] = {'-','.','\t',' '};
+char targetchars[5] = {'-','.','\t',' ',':'};
+
+
 int main(int argc, char* argv[]){
-	/*
 	FILE* makefile = fopen("./uMakefile", "r");
 
 	size_t  bufsize = 0;
 	char*   line    = NULL;
 	ssize_t linelen = getline(&line, &bufsize, makefile);
-	int type = 0;
+	//int type = 0;
 
 	while(-1 != linelen) {
 
@@ -31,12 +36,14 @@ int main(int argc, char* argv[]){
 			line[linelen] = '\0';
 		}
 
+
+
+		printf("%d\n", linetype(line));
+		//printchars(line);
 		linelen = getline(&line, &bufsize, makefile);
 
-		type = linetype(line);
-
-		printf("%d\n",type);
 	}
+	printf("\n%d\n", validchar(':',LINE_TARGET));
 
 	free(line);
 	fclose(makefile);
@@ -46,28 +53,61 @@ int main(int argc, char* argv[]){
 
 int linetype(char* thischar){
 	if(*thischar == ' ')
-		return linetype(thischar++);
+		return linetype(++thischar);
 	else if(*thischar == '\t')
-		return linetype_rule(thischar++);
+		return linetype_rule(++thischar);
 	else if(isalnum(*thischar))
-		return linetype_targ(thischar++);
+		return linetype_targ(++thischar);
 	else return LINE_INVALID;
 }
 
 int linetype_rule(char* thischar){
-	if(isalnum(*thischar) || *thischar == ' ' || *thischar == '\t')
-		return linetype_rule(thischar++);
+	if(isalnum(*thischar) || *thischar == ' ' || *thischar == '\t' || *thischar == ':' || *thischar == '.')
+		return linetype_rule(++thischar);
 	else if(*thischar == '\0')
 		return LINE_RULE;
 	else
-		return LINE_INVALID;
+		return LINE_RULE;
 }
 
 int linetype_targ(char* thischar){
-	if(isalnum(*thischar) || *thischar == ' ' || *thischar == '\t')
-		return linetype_targ(thischar++);
+	if(isalnum(*thischar) || *thischar == ' ' || *thischar == '\t' || *thischar == ':' || *thischar == '.')
+		return linetype_targ(++thischar);
 	else if(*thischar == '\0')
 		return LINE_TARGET;
-	else
-		return LINE_INVALID;
+	else{
+		//printf("",*thischar);
+		return LINE_TARGET;
+	}
+}
+
+bool validchar(char thischar, int charset){
+	char* validchars;
+	switch(charset){
+		case LINE_TARGET:
+			validchars = targetchars;
+			break;
+		case LINE_RULE:
+			validchars = rulechars;
+			break;
+		default:
+			perror("validchar: invalid charset");
+			break;
+	}
+
+	bool contains = false;
+	for(int i=0; i<sizeof(validchars)/sizeof(char); i++){
+		if(validchars[i] == thischar) contains = true;
+	}
+	return contains;
+}
+
+void printchars(char* chars){
+	if(*chars != '\0'){
+		printf("%c",*chars);
+		//chars++;
+		printchars(++chars);
+	}else{
+		printf("done\n");
+	}
 }
