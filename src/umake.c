4abcca0bc3639e8c1e6cdf441eb54390b754190d
Joe Gildner <joegildner0@gmail.com>
2018-11-20 22:37:37 -0800

    Two major changes. Simplified the lineparser and added it into umake.c. Also fixed environment variable declaration parsing in umake.c

--- src/umake.c
+++ src/umake.c
@@ -14,7 +14,6 @@
 
 #include "arg_parse.h"
 #include "targets.h"
-#include "linetype.h"
 
 /* CONSTANTS */
 
@@ -25,6 +24,19 @@
 int getlinetype(char* line);
 
 int addenvvar(char* line);
+
+int linetype(char* thischar);
+int linetype_q1(char* thischar);
+int linetype_rule(char* thischar);
+int linetype_targ(char* thischar);
+int linetype_var(char* thischar);
+
+enum{
+  LINE_INVALID = -1,
+  LINE_TARGET,
+  LINE_RULE,
+  LINE_VAR
+};
 
 /* Main entry point.
 * argc    A count of command-line arguments
@@ -90,10 +102,92 @@ int main(int argc, char* argv[]) {
  * the current environment
  */
 int addenvvar(char* line){
-  char* tokenptr;
+  char name[32];
+  int iname = 0;
+  char val[32];
+  int ival = 0;
 
-  char* name = strtok_r(line, " = ", &tokenptr);
-  char* val = strtok_r(NULL, " = ", &tokenptr);
+  while(*line != '='){
+    if(*line != ' '){
+      name[iname] = *line;
+      name[iname+1] = '\0';
+      iname++;
+    }
+    line++;
+  }
+  line++;
+
+  bool leadspace = (*line == ' ');
+  while(*line != '\0'){
+    if(*line == ' ' && leadspace)
+      line++;
+    else if(*line != ' ' && leadspace)
+      leadspace = false;
+    else{
+      val[ival] = *line;
+      val[ival+1] = '\0';
+      ival++;
+      line++;
+    }
+  }
 
   return setenv(name, val, 0);
+}
+
+/* linetype
+ * thischar   current character in line
+ * the following give functions represent states on the above statemachine, except invalid.
+ * the functions call eachother based on given input and follow the edges of the state machine
+ */
+int linetype(char* thischar){
+  if(*thischar == ' ')
+    return linetype(++thischar);
+  else if(*thischar == '\t')
+    return linetype_rule(++thischar);
+  else if(isalnum(*thischar))
+    return linetype_q1(++thischar);
+  else return LINE_INVALID;
+}
+
+int linetype_q1(char* thischar){
+  if(*thischar == ':')
+    return linetype_targ(++thischar);
+  else if(*thischar == '=')
+    return linetype_var(++thischar);
+  else 
+    return linetype_q1(++thischar);
+}
+
+int linetype_targ(char* thischar){
+  if(*thischar == '#'){
+    *thischar = '\0';
+    return LINE_TARGET;
+  }
+  else if(*thischar == '\0')
+    return LINE_TARGET;
+  else
+    return linetype_targ(++thischar);
+}
+
+int linetype_rule(char* thischar){
+  if(*thischar == '#'){
+    *thischar = '\0';
+    return LINE_RULE;
+  }
+  else if(*thischar == '\0')
+    return LINE_RULE;
+  else
+    return linetype_rule(++thischar);
+}
+
+
+int linetype_var(char* thischar){
+  if(*thischar == '#'){
+    *thischar = '\0';
+    return LINE_VAR;
+  }
+  else if(*thischar == '\0')
+    return LINE_VAR;
+  else
+    return linetype_var(++thischar);
 }
